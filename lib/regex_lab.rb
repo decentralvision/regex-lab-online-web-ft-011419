3ea5760ca26590d6de2a6585396a06c46a5363eb
Thomas Kunkel <thomas.boyd.kunkel@gmail.com>
2019-01-15 02:06:24 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,7 +1,5 @@
 def starts_with_a_vowel?(word)
-  puts word
-  puts word.scan(/\A([aeiou]|[AEIOU])/).empty?
-  word.scan(/\A[aeiou]/) ? true : false
+  word.scan(/\A([aeiou]|[AEIOU])/).empty? ? false : true
 end
 
 def words_starting_with_un_and_ending_with_ing(text)

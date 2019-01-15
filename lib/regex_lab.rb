97ea7df9ef0fbb66d83b093381611cb14316f546
Thomas Kunkel <thomas.boyd.kunkel@gmail.com>
2019-01-15 01:57:25 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,6 +1,6 @@
 def starts_with_a_vowel?(word)
   puts word.scan(/\A[aeiou]/).class
-  word.scan(/\A[aeiou]/) ? true : false
+  word.scan(/\A[aeiou]/).empty? ? true : false
 end
 
 def words_starting_with_un_and_ending_with_ing(text)

cb97e6f4d9bdaf87601b59753ae1da2af569c79c
Thomas Kunkel <thomas.boyd.kunkel@gmail.com>
2019-01-15 01:47:50 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,7 +1,5 @@
 def starts_with_a_vowel?(word)
-   if word.scan(/\b[aeiou]/)
-     true
-   end
+   word.scan(/\b[aeiou]/) ? true : false
 end
 
 def words_starting_with_un_and_ending_with_ing(text)

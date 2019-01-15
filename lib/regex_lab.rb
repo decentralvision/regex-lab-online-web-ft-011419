e83672f497881961e0643b25ff7ea4d612de2731
Thomas Kunkel <thomas.boyd.kunkel@gmail.com>
2019-01-15 01:57:49 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,6 +1,6 @@
 def starts_with_a_vowel?(word)
   puts word.scan(/\A[aeiou]/).empty?
-  word.scan(/\A[aeiou]/).empty? ? false : true
+  word.scan(/\A[aeiou]/) ? false : true
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
